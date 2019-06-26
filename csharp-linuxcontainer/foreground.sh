7ff53089bb2a36372ca6578f7772b457aa7e4c71
Takayoshi Tanaka <tanaka-takayoshi@users.noreply.github.com>
2019-06-26 10:07:38 -0700

    Use curl instead of wget.

--- csharp-linuxcontainer/foreground.sh
+++ csharp-linuxcontainer/foreground.sh
@@ -1,6 +1,6 @@
-wget https://download.visualstudio.microsoft.com/download/pr/5e92f45b-384e-41b9-bf8d-c949684e20a1/67a98aa2a4e441245d6afe194bd79b9b/dotnet-sdk-2.2.300-linux-x64.tar.gz
+curl https://download.visualstudio.microsoft.com/download/pr/5e92f45b-384e-41b9-bf8d-c949684e20a1/67a98aa2a4e441245d6afe194bd79b9b/dotnet-sdk-2.2.300-linux-x64.tar.gz --output dotnet-sdk.tar.gz
 
-mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-2.2.300-linux-x64.tar.gz -C $HOME/dotnet
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk.tar.gz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet
 export PATH=$PATH:$HOME/dotnet
 
